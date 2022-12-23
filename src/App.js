dc5231b9f70b3d56faef9290ee572db6ce79ffd8
mayank dutta <smayankdutt@gmail.com>
2022-12-23 16:20:38 +0530

    done with structure, styling pending

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import Papa from "papaparse";
+import { useEffect, useState } from "react";
+import "./App.css";
+
+const URL =
+  "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ2aYIhdBH95KsM-3ohuHeGiPIgd2KzlBtT2CXoqJwVXj3SDiA3-xVu-VDhWxtjETiPWejkXa3lEtpB/pub?output=csv";
 
 function App() {
+  const [table, setTable] = useState([]);
+  const [tableHeadRow, setTableHeadRow] = useState([]);
+
+  useEffect(() => {
+    Papa.parse(URL, {
+      download: true,
+      header: true,
+      complete: (results) => {
+
+        setTable(results.data);
+
+        for (let j in results.data[0]) {
+          setTableHeadRow((prev) => [...prev, j]);
+        }
+      },
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {tableHeadRow && table ? (
+        <table>
+          <thead>
+            <tr>
+              {tableHeadRow.map((value, index) => {
+                return <th key={index}>{value}</th>;
+              })}
+            </tr>
+          </thead>
+
+          <tbody>
+            {table.map((value, index) => (
+              <tr key={index}>
+                {tableHeadRow.map((subValue) => (
+                  <td>{value[subValue]}</td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <h1> Loading ... </h1>
+      )}
+    </>
   );
 }
 
